18cb8af7139fbdff7fee67d52b997a55438875ce
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-04-08 19:34:02 +0300

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config('../.env');
+
+module.exports = {
+    port: process.env.PORT,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+};
