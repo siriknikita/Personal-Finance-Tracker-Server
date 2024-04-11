3353b25416ba9df57287acc1e7045611fce87ddf
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-04-11 23:41:42 +0300

    Update database.js to add SSL configuration for MySQL connection

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
-require("dotenv").config();
 const mysql = require('mysql2')
+const fs = require('fs');
+require("dotenv").config();
 
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(process.cwd() + '/BaltimoreCyberTrustRoot.crt.pem')
+    }
 }).promise();
 
 async function getUser(email) {
