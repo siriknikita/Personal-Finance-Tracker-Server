da50c7d7ef3550bc4dcc99adfcb3a7ed6f6b95aa
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-05-31 00:51:32 +0300

    fix; Typo fix in budget's user controller funciton

--- controllers/budget.controller.js
+++ controllers/budget.controller.js
@@ -1,7 +1,7 @@
 const moment = require("moment");
 const { Budget } = require("../models");
 const { sendBudgetLimitExceededEmail } = require("./sendEmail");
-const { userController } = require("./user.controller");
+const userController = require("./user.controller");
 
 const createUserBudget = async (userID) => {
   try {
